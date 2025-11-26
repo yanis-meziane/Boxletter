3cfcd5cacdcd7eee956235fc513a9ef24ce4f0c4
Yanis Meziane <yanismeziane04@icloud.com>
2025-11-26 11:57:49 +0100

    Création du login et du register

--- src/login/loginUser.jsx
+++ src/login/loginUser.jsx
@@ -1,7 +1,26 @@
+import { Link } from "react-router-dom"
+
 export default function Login() {
     return (
         <div>
-            <h1>Ceci est la page login</h1>
+             <form action="" method="post">
+                   
+                    <label htmlFor="mail">Mail : </label>
+                    <input type="email" name="mail" id="mail" placeholder="Votre mail... " minLength={1} maxLength={30}/>
+
+            <br />
+            <br />
+                    <label htmlFor="Mot de passe">Mot de passe : </label>
+                    <input type="password" name="password" id="password" required min={8} placeholder="Votre mot de passe..."/>
+            <br />
+            <br />
+
+                    <input type="button" value="Valider" id="registerButton" />
+
+                    <p>
+                        Si vous n'avez pas de compte, <Link to="/">inscrivez vous</Link>
+                    </p>
+                </form>
         </div>
     );
 }
--- src/register/register.jsx
+++ src/register/register.jsx
@@ -1,8 +1,33 @@
+import { Link } from "react-router-dom"
 export default function Register(){
     return(
         <div>
             <p>
-                Ceci est la page register
+                <form action="" method="post">
+                    <label htmlFor="firstname">Prénom : </label>
+                    <input type="text" name="firstname" id="lastname" placeholder="Votre prénom..." minLength={1} maxLength={30}/>
+            <br />
+            <br />
+                    <label htmlFor="lastname">Nom de famille : </label>
+                    <input type="text" name="lastname" id="lastname" placeholder="Votre nom de famille..." minLength={1} maxLength={30}/>
+            <br />
+            <br />
+                    <label htmlFor="mail">Mail : </label>
+                    <input type="email" name="mail" id="mail" placeholder="Votre mail... " minLength={1} maxLength={30}/>
+
+            <br />
+            <br />
+                    <label htmlFor="Mot de passe">Mot de passe : </label>
+                    <input type="password" name="password" id="password" required min={8} pattern="^(?=.*[a-z0-9])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$" title="Doit contenir au minimum 8 caractères avec 1 majuscule, une miniscule, un caractère spécial et un chiffre" placeholder="Votre mot de passe..." />
+            <br />
+            <br />
+
+                    <input type="button" value="Valider" id="registerButton" />
+
+                    <p>
+                        Si vous avez déjà un compte, <Link to="/login">connectez vous</Link>
+                    </p>
+                </form>
             </p>
         </div>
     )
